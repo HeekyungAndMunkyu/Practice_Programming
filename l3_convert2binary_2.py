3110e567c3270f8b757cac626f083c491bff7256
heekyungyoon <yhk00323@naver.com>
2015-01-13 19:24:33 +0900

    convert2binary programs, hastags attached.

--- l3_convert2binary_2.py
+++ l3_convert2binary_2.py
@@ -1,22 +1,28 @@
-#convert to binary _ different version
+#convert to binary_modified by me
 
 num = int(raw_input('Enter an integer: '))
-
 binary = ''
 negative = False
 
+#there are three cases: positive, zero, negative
+#positive and negative can be computed in the same way except for the (+/-) sign
 
 if num == 0:
 	binary = '0'
 
+#this is how we treat (+/-) sign problem.
 if num < 0:
 	num = abs(num)
 	negative = True
 
+
+#positive and negative are computed the same way.
 while num >= 1: 
 	binary = str(num%2) + binary
 	num /= 2
 
+
+#a final touch on the (+/-) sign
 if negative:
 	binary = '-' + binary
 
