4e336a80f870d550079ca09a79e208c7fbe7b5c6
heekyungyoon <yhk00323@naver.com>
2015-01-14 09:20:37 +0900

    l3_p9.py: added #my code_2

--- l3_p9.py
+++ l3_p9.py
@@ -44,6 +44,7 @@ print 'Game over. Your secret number was: ', guess
 
 
 #SAMPLE CODE
+print ''
 print 'SAMPLE CODE'
 print("Please think of a number between 0 and 100!")
 
@@ -72,3 +73,43 @@ while not guessed:
         print("Sorry, I did not understand your input.")
 
 print('Game over. Your secret number was: ' + str(guess))
+
+
+
+
+
+
+
+#my code_2
+print ''
+print 'My code_2'
+print 'Please think of a number between 0 and 100!'
+ans = ''
+high = 100
+low = 0
+guess = 50
+
+
+
+while ans != 'c':
+        print 'Is your secret number ', guess, '?'
+        print "Enter 'h' to indicate the guess is too high. ",
+        print "Enter 'l' to indicate the guess is too low. ",
+        print "Enter 'c' to indicate I guessed correctly. ",
+        ans = raw_input ()
+
+        if ans == 'c':
+                break
+        elif ans == 'h':
+                high = guess
+        elif ans == 'l':
+                low = guess
+	else:
+		print 'Sorry, I did not understand your input.'
+
+        guess = (high + low)/2
+
+
+
+print 'Game over. Your secret number was: ', guess
+
