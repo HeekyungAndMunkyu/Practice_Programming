505ef4fc44edca5e02c3c6f287b7267deae50203
heekyungyoon <yhk00323@naver.com>
2015-02-06 21:32:38 +0900

    intoTuple complete

--- spiralnumber2.py
+++ spiralnumber2.py
@@ -12,7 +12,7 @@ def spiralNumber(row, col):
     # merge the tuple list and the number list into a dictionary
 
     # print out the dictionary in a spiral fashion
-    printNum(intoDict(intoTuple(row, col)))
+    printNum(intoDict(intoTuple(row, col)), row)
 
 def intoTuple(row, col):
     '''
@@ -27,24 +27,78 @@ def intoTuple(row, col):
     rowFixed = True # False: columm fixed
     increasing = True # False: decreasing
 
-    # when row index is fixed
-    if rowFixed:
-        # columm index in increasing order (left -> right)
-        if increasing:
+    lastRow = 0
+    lastCol = -1
 
-        # columm index in decreasing order (right -> left)
+    keyList = []
+
+    rowDifList = range(rowIndex, -1, -1)
+    colDifList = range(colIndex + 1, -1, -1)
+    rowDif = 0
+    colDif = 0
+
+    print 'rowDifList:', rowDifList, '       colDifList:', colDifList
+
+    while len(keyList) < row * col:
+        # when row index is fixed
+        if rowFixed:
+            print 'rowFixed'
+            colDif = colDifList.pop(0)
+            # columm index in increasing order (left -> right)
+            if increasing:
+                lastRow, lastCol, keyList = tupGenRowFixed(lastRow, lastCol, colDif, keyList, increasing)
+            # columm index in decreasing order (right -> left)
+            else:
+                lastRow, lastCol, keyList = tupGenRowFixed(lastRow, lastCol, colDif, keyList, increasing)
+            rowFixed = False
+        # when columm index is fixed
         else:
+            print 'colFixed'
+            rowDif = rowDifList.pop(0)
+            # row index in increasing order (top -> bottom)
+            if increasing:
+                lastRow, lastCol, keyList = tupGenColFixed(lastRow, lastCol, rowDif, keyList, increasing)
+                increasing = False
+            # row index in decreasing order (bottom -> top)
+            else:
+                lastRow, lastCol, keyList = tupGenColFixed(lastRow, lastCol, rowDif, keyList, increasing)
+                increasing = True
+            rowFixed = True
+    return keyList
 
-    # when columm index is fixed
+
+def tupGenRowFixed(lastRow, lastCol, colDif, keyList, increasing):
+    '''
+    '''
+    print 'lastRow =  ', lastRow, '  lastCol=  ', lastCol, '  colDif =  ', colDif
+    for i in range(colDif):
+        if increasing:
+            keyList.append((lastRow, lastCol + 1 + i))
+
+        else:
+            keyList.append((lastRow, lastCol - 1 - i))
+    if increasing:
+        lastCol = lastCol + colDif
     else:
-        # row index in increasing order (top -> bottom)
-        if increasing:
-            increasing = False
-        # row index in decreasing order (bottom -> top)
-        else:
-            increasing = True
-def
+        lastCol = lastCol - colDif
+    print keyList
+    return lastRow, lastCol, keyList
 
+def tupGenColFixed(lastRow, lastCol, rowDif, keyList, increasing):
+    '''
+    '''
+    print 'lastRow =  ', lastRow, '  lastCol=  ', lastCol, '  rowDif =  ', rowDif
+    for i in range(rowDif):
+        if increasing:
+            keyList.append((lastRow + 1 + i, lastCol))
+        else:
+            keyList.append((lastRow - 1 - i, lastCol))
+    if increasing:
+        lastRow = lastRow + rowDif
+    else:
+        lastRow = lastRow - rowDif
+    print keyList
+    return lastRow, lastCol, keyList
 
 def intoDict(keyList):
     '''
@@ -58,3 +112,16 @@ def intoDict(keyList):
         numDict[keyList[i]] = i
 
     return numDict
+
+def printNum(numDict, row):
+    '''
+    return: prints
+    '''
+#    for i in range(row):
+#        print numDict[(i, *)]
+
+row = int(raw_input('row:  '))
+col = int(raw_input('columm:  '))
+
+for i in range(row):
+    print intoTuple(row, col)[i:i+col]
