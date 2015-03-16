4fac453b0dc691ed8b3a84d0a42c0752aa2f8ec9
heekyungyoon <yhk00323@naver.com>
2015-03-17 00:28:12 +0900

    added api key

--- nytApi.py
+++ nytApi.py
@@ -1,1 +1,10 @@
-http://http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&begin_date=20120101&end_date=20120101&api-key=####
+import urllib2
+import json
+
+request_string = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=data+science&facet_field=\
+day_of_week&begin_date=20120101&end_date=20120101&api-key=99d7b651ac8108a2a9ed799af4b3b467:1:71594713'
+
+file = urllib2.urlopen(request_string).read()
+
+decoded = json.loads(file)
+print decoded
