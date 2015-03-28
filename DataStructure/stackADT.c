b108141c3e1475c7f8c65a4e05dff13c3a17a173
heekyungyoon <yhk00323@naver.com>
2015-03-28 10:48:47 +0900

    STACK* and more

--- DataStructure/stackADT.c
+++ DataStructure/stackADT.c
@@ -1,25 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-// Data type
-typedef struct node {
+// Structure Declarations
+typedef struct node
+  {
   void* dataPtr;
-  STACK_NODE* link;
-} STACK_NODE;
+  struct node* link;
+  } STACK_NODE;
 
-typedef struct node {
+typedef struct
+  {
   int count;
   STACK_NODE* top;
-} STACK;
+  } STACK;
 
-// Data operations
-STACK createStack () {
+// Prototype Declarations
+STACK* createStack (void);
+bool pushStack (STACK* stack, void* dataInPtr);
+void* popStack (STACK* stack);
+void* stackTop (STACK* stack);
+bool emptyStack (STACK* stack);
+bool fullStack (STACK* stack);
+int stackCount (STACK* stack);
+STACK* destroyStack (STACK* stack);
+
+
+/* =============== createStack ==================
+  Creates an empty stack
+    Pre Nothing
+    Post Returns pointer to a null stack
+      -or- NULL if overflow
+*/
+STACK* createStack (void)
+{
   // Local Declarations
-  STACK stack;
+  STACK* stack;
 
   //
-  stack = (STACK) malloc(size of (STACK))
-  stack->count = 0;
-  stack->top = NULL;
+  stack = (STACK*) malloc(sizeof (STACK))
+  if (stack)
+    {
+      stack->count = 0;
+      stack->top = NULL;
+    } // if
 
   return stack;
-}
+} // createStack
