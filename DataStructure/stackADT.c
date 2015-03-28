e0468c247bb63726a610c5a83ac0f6f57ddaa601
heekyungyoon <yhk00323@naver.com>
2015-03-28 12:40:52 +0900

    popstack

--- DataStructure/stackADT.c
+++ DataStructure/stackADT.c
@@ -50,18 +50,43 @@ STACK* createStack (void)
 
 
 /* ================ pushStack =================
+    Pre stack: STACK*
+        dataInPtr: data* to be inserted
 
+    Post data inserted into stack
+    Return true if successful; flase if underflow
 
 */
-void pushStack (STACK* stack, void* dataInPtr)
+bool pushStack (STACK* stack, void* dataInPtr)
 {
   // Local Declarations
   STACK_NODE* newPtr;
 
   // Statements
-  newPtr = (STACK_NODE*) malloc(sizeof(STACK_NODE))
-  newPtr->dataPtr = dataInPtr
-  newPtr->link = stack->top
-  stack->top = newPtr
-  return 0;
+  newPtr = (STACK_NODE*) malloc(sizeof(STACK_NODE));
+  if (!newPtr)
+    return false;
+
+  newPtr->dataPtr = dataInPtr;
+  newPtr->link = stack->top;
+  stack->top = newPtr;
+
+  (stack->count)++;
+  return true;
 } // pushStack
+
+
+/* ================ popStack ====================
+
+*/
+void* popStack (STACK* stack)
+{
+  //Local Definitions
+  void* dataOutPtr;
+  //Statements
+  dataOutPtr = stack->top;
+  stack->top = stack->top->link;
+  stack->top->link = NULL;
+  
+  return dataOutPtr;
+}
