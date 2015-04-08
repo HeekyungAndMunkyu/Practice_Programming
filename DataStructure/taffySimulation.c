1b011cf1cecfe9f2e771992fd0688d04bc87e01d
heekyungyoon <yhk00323@naver.com>
2015-04-09 00:05:53 +0900

    fix

--- DataStructure/taffySimulation.c
+++ DataStructure/taffySimulation.c
@@ -57,7 +57,7 @@ int main (void)
 		newCustomer (queue, clockTime, custNum);
 		int countcount;
 		countcount = queueCount (queue);
-		printf ("clockTime:%d, custNum: %d, queueCount:%d" % (*clockTime, *custNum, countcount));
+		printf ("clockTime:%d, custNum: %d, queueCount:%d", *clockTime, *custNum, countcount);
 		}
 		/* serverFree (queue, clockTime, status, moreCusts);
 		svcComplete (queue, clockTime, status, stats, moreCusts);
