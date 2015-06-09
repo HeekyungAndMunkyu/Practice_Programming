2638feefb707235418436600f7e8fd66d538737b
heekyungyoon <yhk00323@naver.com>
2015-06-09 23:09:19 +0900

    line149,15-,151

--- DataStructure/pageRank/Source Code/pagerank_matrix.c
+++ DataStructure/pageRank/Source Code/pagerank_matrix.c
@@ -142,11 +142,13 @@ void loadAdjacencyMatrix(char* inputFile, double** matrix) {
 		//test
 		printf("%s", element);
 		//
-		matrix[i][j++] = (double)atoi(element);
+		matrix[i][j] = (double)atoi(element);
 		//test
 		printf("loadAdjacency matrix[%d][%d]\n", i, j);
 		//
-		while (element = strtok(NULL, " ")) {
+		//while (element = strtok(NULL, " ")) {
+		while (element) {
+			element = strtok(NULL, " ");
 			matrix[i][j++] = (double)atoi(element);
 			printf("%s", element);
 			printf("loadAdjacency matrix[%d][%d]\n", i, j);
